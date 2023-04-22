00db9f2408c5d2875b9c8593ac71617319e96efa
Rohit Yadav <rty2423@outlook.com>
2023-04-22 10:49:57 +0000

    initial file commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelector("button").addEventListener("click",function handleClick(){
+    alert("I got clicked!!!");
+    var audio=new Audio('Sweet.mp3');
+    audio.play()});
+document.getElementsByClassName("button1").click();
+   var loveScore=Math.random()*100;
+   loveScore=Math.floor(loveScore)+1;
+   alert("Your loveScore is " + loveScore);
