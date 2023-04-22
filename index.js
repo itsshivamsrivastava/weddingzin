8f5dfafaa54fd64e591d58be9a1d8f1e24fa7033
Shivam Srivastava <73056786+itsshivamsrivastava@users.noreply.github.com>
2023-04-22 17:34:18 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 
-document.querySelector("button").addEventListener("click",function handleClick(){
-    alert("I got clicked!!!");
-    var audio=new Audio('Sweet.mp3');
-    audio.play()});
-document.getElementsByClassName("button1").click();
-   var loveScore=Math.random()*100;
-   loveScore=Math.floor(loveScore)+1;
-   alert("Your loveScore is " + loveScore);
+// document.querySelector("button").addEventListener("click",function handleClick(){
+//     alert("I got clicked!!!");
+//     var audio=new Audio('Sweet.mp3');
+//     audio.play()});
+// document.getElementsByClassName("button1").click();
+//    var loveScore=Math.random()*100;
+//    loveScore=Math.floor(loveScore)+1;
+//    alert("Your loveScore is " + loveScore);
+
+// Create a node server to host the website
+// npm init -y
+// npm install express
+// npm install body-parser
+// npm install request
+
+// Path: index.js
+
+const express=require("express");
+const bodyParser=require("body-parser");
+// const request=require("request");
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+});
